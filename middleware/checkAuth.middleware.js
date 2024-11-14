c959bea2e2790e9c000bbd757fc6f81d4c47bddf
sandeshgm <sandeshgm3232@gmail.com>
2024-11-14 23:25:59 +0545

    order table

--- middleware/checkAuth.middleware.js
+++ middleware/checkAuth.middleware.js
@@ -3,14 +3,14 @@ const { JWT_SECRET_KEY } = require("../config/constants");
 
 const checkAuth = (req, res, next) => {
   console.log(req.cookies.token);
-  const { token } = req.cookies.token ?? req.headers.token;
+  const  token  = req.cookies.token ?? req.headers.token;
   try {
     const user = jwt.verify(token, JWT_SECRET_KEY);
     req.authUser = user;
     next();
   } catch (error) {
     res.status(400).json({
-      message: "Unauthorized access",
+      message: "Unauthorized",
     });
   }
 };
