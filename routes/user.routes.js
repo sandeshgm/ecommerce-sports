2c73687fda859465a4c3df3e48da23de52e3dabc
sandeshgm <sandeshgm3232@gmail.com>
2024-11-23 22:36:27 +0545

    user crud

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,13 +3,16 @@ const {
   getUser,
   updateUser,
   deleteUser,
+  getSingleUser,
 } = require("../controllers/userControllers/user.controllers");
 const { checkAuthAdmin } = require("../middleware/checkAuth.middleware");
 const userGetValidate = require("../validation/User.validation.js/userGet.validator");
 
 const router = express.Router();
 
+
 router.get("/", userGetValidate, checkAuthAdmin, getUser);
+router.get("/:id", userGetValidate, checkAuthAdmin, getSingleUser);
 router.patch("/:id", updateUser);
 router.delete("/:id", checkAuthAdmin, deleteUser);
 
